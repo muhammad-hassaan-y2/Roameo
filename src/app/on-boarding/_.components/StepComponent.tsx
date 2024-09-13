3d58d8ee9eb3880a0f95a749b84c156e9b6a41e6
Muhammad Hassaan Shaukat <mr.hassaan50@gmail.com>
2024-09-13 23:10:13 +0500

    Dashboard setup

--- src/app/on-boarding/_.components/StepComponent.tsx
+++ src/app/on-boarding/_.components/StepComponent.tsx
@@ -1,4 +1,3 @@
-// components/StepContent.tsx
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Upload, Map, Users, Lightbulb, DollarSign } from 'lucide-react'
